a8ee830f4492db6d763c17efc8e3f9fdb4db5ba8
FRC <frc3316@gmail.com>
2016-01-14 17:09:47 +0200

    Trying to add external jars

--- Robot Code/src/org/usfirst/frc/team3316/robot/robotIO/Sensors.java
+++ Robot Code/src/org/usfirst/frc/team3316/robot/robotIO/Sensors.java
@@ -7,13 +7,19 @@ import org.usfirst.frc.team3316.robot.Robot;
 import org.usfirst.frc.team3316.robot.config.Config;
 import org.usfirst.frc.team3316.robot.logger.DBugLogger;
 
+import edu.wpi.first.wpilibj.SerialPort;
+
 public class Sensors 
 {
 	Config config = Robot.config;
 	DBugLogger logger = Robot.logger;
 	
+	public IMUAdvanced navx;
+	SerialPort serial_port;
+	
 	public Sensors ()
 	{
-		
+		serial_port = new SerialPort(57600, SerialPort.Port.kMXP);
+		navx = new IMUAdvanced(serial_port);
 	}
 }
