8bc0098014ab4149e028a3a8791672415dd31037
LeonAgmonNacht <ktuitb@gmail.com>
2016-01-18 23:19:34 +0200

    Commit by Leon, updating static vars

--- Raspberry Pi/PythonCV.py
+++ Raspberry Pi/PythonCV.py
@@ -6,18 +6,18 @@ from VisionManager import *
 from NetworkManager import *
 import cv2
 import numpy as np
-LB = np.array([110,50,50])
-UB = np.array([130,255,255])
+LB = np.array([0,0,250])
+UB = np.array([255,0,255])
 MBR = 500 # Minimum bounding rect.
 KH = 36 # Known height. (of U).
-KW = 36 # Known width.  (of U).
+KW = 50.8 # Known width.  (of U).
 FL = 1400 # Focal length.
-RW = 70 # Robot width.
-RH = 300 # Robot height.
-RL = 70 # Robot length.
-TH = 305 # The height of the tower
-CUW = 100 # (Center U Width) The U width as it looks in the camera when it is in the center (in pixels).
-CUWD = 100 # The distance which the CUW was calculated from.
+RW = 100 # Robot width.
+RH = 0 # Robot height.
+RL = 100 # Robot length.
+TH = 40 # The height of the tower
+CUW = 200 # (Center U Width) The U width as it looks in the camera when it is in the center (in pixels).
+CUWD = 240 # The distance which the CUW was calculated from.
 cam = cv2.VideoCapture(0)
 visionManager = VisionManager(LB,UB,MBR,cam,KH,KW,FL,[RH,RW,RL],TH, CUW, CUWD)
 #networkManager = NetworkManager("localhost",8080)
@@ -26,13 +26,13 @@ while True:
     visionManager.updateImage()
     visionManager.updateTowerScales()
     visionManager.updateRobotScales()
-    # For Fainaro:
-    # if visionManager.currentImageObject is not None:
-    #     (x,y,h,w) = (visionManager.currentImageObject.objectX,
-    #                  visionManager.currentImageObject.objectY,
-    #                  visionManager.currentImageObject.objectHeight,
-    #                  visionManager.currentImageObject.objectWidth)
-    #     cv2.rectangle(visionManager.maskedImage, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    #For Fainaro:
+    if visionManager.currentImageObject is not None:
+        (x,y,h,w) = (visionManager.currentImageObject.objectX,
+                     visionManager.currentImageObject.objectY,
+                     visionManager.currentImageObject.objectHeight,
+                     visionManager.currentImageObject.objectWidth)
+        cv2.rectangle(visionManager.maskedImage, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # Send data to java process:
     #if visionManager.currentImageObject is not None:
@@ -42,7 +42,7 @@ while True:
     #                             visionManager.robotObject.Yposition])
 
     # Print results
-    #if visionManager.currentImageObject is not None:
+    if visionManager.currentImageObject is not None:
         #print("D",visionManager.currentImageObject.distanceFromCamera)
         #print("A",visionManager.robotObject.angle)
         #print("X",visionManager.robotObject.XPosition)
@@ -50,7 +50,9 @@ while True:
         #print("XShift",visionManager.currentImageObject.XShift)
         #print("YShift",visionManager.currentImageObject.YShift)
 
-    cv2.imshow("Current Image",visionManager.maskedImage)
+        cv2.imshow("Current Image",visionManager.maskedImage)
+        print(visionManager.currentImageObject.objectWidth)
+    #cv2.imshow("c", visionManager.currentImage)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
