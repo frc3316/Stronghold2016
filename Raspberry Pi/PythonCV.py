203d3707c65dd95eed7f9c2ee015cce90314a687
PI <fr3316@gmail.com>
2016-01-22 08:56:54 +0000

    commenting (A LOT OF COMMENTING) y u no comment Leon ;-;

--- Raspberry Pi/PythonCV.py
+++ Raspberry Pi/PythonCV.py
@@ -1,8 +1,6 @@
 '''
-    This is the main module that handles and uses all other modules for the Python CV in the First Robotics Competition 2016.
-    Created by: Leon Agmon Nacht.
-    '''
-
+This is the main module that handles and uses all other modules for the Python CV
+'''
 from VisionManager import *
 from NetworkManager import *
 from FPSCounter import *
@@ -10,94 +8,130 @@ from time import sleep
 import cv2
 import numpy as np
 
-LB = np.array([37,0,234])
-UB = np.array([120,73,255])
-# LB = np.array([75,230,235]) # Lower bond.
-# UB = np.array([105,255,255]) # Upper bond.
+#############
+# constants #
+#############
+
+#Colors:
+LB = np.array([37,0,234]) # Lower bond
+UB = np.array([120,73,255]) # Upper bond
+
+# Bounding rectangle
 MBR = 500 # Minimum bounding rectangle
 KH = 36 # Known height (of U).
 KW = 50.8 # Known width  (of U).
 FL = 1125 # Focal length.
+
+# Robot:
 RW = 100 # Robot width.
 RH = 0 # Robot height.
 RL = 100 # Robot length.
+
+# Field:
 TH = 50 # The height of the tower
 CUW = 260 # (Center U Width) The U width as it looks in the camera when it is in the center (in pixels).
 CUWD = 220 # The distance which the CUW was calculated from.
+
+# Camera:
 HAX = 53 #The head angle of the camera (x)
 HAY = 40 #The head angle of the camera (y)
 
+##################################
+# Camera and FPS counter setting #
+##################################
+
 FPSCounter = FPS()
 FPSCounter.start()
+
+cam = cv2.VideoCapture(0)
+
 brightness = -0.1
 saturation = 15
-exposure = -1
-cam = cv2.VideoCapture(0)
+exposure = -1 
+
 cam.set(cv2.cv.CV_CAP_PROP_BRIGHTNESS, brightness)
 cam.set(cv2.cv.CV_CAP_PROP_SATURATION, saturation)
-cam.set(cv2.cv.CV_CAP_PROP_EXPOSURE, exposure)
+cam.set(cv2.cv.CV_CAP_PROP_EXPOSURE, exposure) # not working on the old camera
 
 visionManager = VisionManager(LB,UB,MBR,cam,KH,KW,FL,[RH,RW,RL],TH, CUW, CUWD)
 #networkManager = NetworkManager("localhost",8080)
+
+###################
+# The code itself #
+###################
 
 while True:
     visionManager.updateImage()
     visionManager.updateTowerScales()
     visionManager.updateRobotScales()
+    
     FPSCounter.update()   
     
-    # For Fainaro:
-    if visionManager.currentImageObject is not None:
+    if visionManager.currentImageObject is not None: # if an object was detected
+
+        ######################
+        # Rectangle creation #
+        ######################
         
         (x, y, h, w) = (visionManager.currentImageObject.objectX,
                      visionManager.currentImageObject.objectY,
                      visionManager.currentImageObject.objectHeight,
-                     visionManager.currentImageObject.objectWidth)
+                     visionManager.currentImageObject.objectWidth)     
+        #(x, y) = top left corner, h+ is down, w+ is right
+        
         cv2.rectangle(visionManager.maskedImage, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    
-    # Send data to java process:
-    #if visionManager.currentImageObject is not None:
+
+        #########################################
+        # Azimuthal and polar angle calculation #
+        #########################################
+        
+        (xO, yO) = (x + w/2, y + h/2) # center of the object
+        (xC, yC) = (visionManager.imageWidth/2, visionManager.imageHeight/2) # center of the frame
+
+        azimuthalAngle = float((xO-xC)*HAX)/float(visionManager.imageWidth) # x, y plane
+        polarAngle = float((yO-yC)*HAY)/float(visionManager.imageHeight) # azimuthal, z plane
+        
+        print (azimuthalAngle, polarAngle)
+
+    #############################
+    # Send data to java process #
+    #############################
+        
+    # if visionManager.currentImageObject is not None:
     #    networkManager.sendData([visionManager.currentImageObject.distanceFromCamera,
     #                             visionManager.robotObject.angle,
     #                             visionManager.robotObject.XPosition,
     #                             visionManager.robotObject.Yposition])
     
-    # Print results
+    ###################
+    # Results printer #
+    ###################
+    
     # if visionManager.currentImageObject is not None:
-        #print("D",visionManager.currentImageObject.distanceFromCamera)
-        #print("Angle: ",visionManager.robotObject.angle)
-        #print("X",visionManager.robotObject.XPosition)
-        #print("Y",visionManager.robotObject.Yposition)
-        #print("XShift",visionManager.currentImageObject.XShift)
-        #print("YShift",visionManager.currentImageObject.YShift)
-        #cv2.putText(visionManager.currentImage, "fps=%s" % (FPSCounter.fps()),
-        #    (10, 75), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
+        # print("D",visionManager.currentImageObject.distanceFromCamera)
+        # print("Angle: ",visionManager.robotObject.angle)
+        # print("X",visionManager.robotObject.XPosition)
+        # print("Y",visionManager.robotObject.Yposition)
+        # print("XShift",visionManager.currentImageObject.XShift)
+        # print("YShift",visionManager.currentImageObject.YShift)
 
+        # put the FPS on the picture
+        # cv2.putText(visionManager.currentImage, "fps=%s" % (FPSCounter.fps()),
+        #       (10, 75), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
+        
     FPSCounter.stop()
     print(FPSCounter.fps())
-        #cv2.putText(visionManager.currentImage, "fps=%s" % (FPSCounter.fps()),\
-        #            (10, 75), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
-
+    
     #display:    
     cv2.imshow("Current Image",visionManager.currentImage)
-    cv2.imshow("Thresh Image", visionManager.maskedImage)
+    cv2.imshow("Masked Image", visionManager.maskedImage)
 
-    if visionManager.currentImageObject is not None:
-        (xA, yA, hA, wA) = (visionManager.currentImageObject.objectX,
-                         visionManager.currentImageObject.objectY,
-                         visionManager.currentImageObject.objectHeight,
-                         visionManager.currentImageObject.objectWidth)
-        #(xA,yA) = top left corner, (xA, yA, hA, wA) = the center of the object
+    #########################
+    # Wait for key pressing #
+    #########################
 
-        (xO, yO) = (xA + wA/2, yA + hA) # center of the object
-        (xC, yC) = (visionManager.imageWidth/2, visionManager.imageHeight/2) #middle of the frame
-
-        azimuthalAngle = float((xO-xC)*HAX)/float(visionManager.imageWidth)
-        polarAngle = float((yO-yC)*HAY)/float(visionManager.imageHeight)
-        
-        print (azimuthalAngle, polarAngle)
-        
-
+    # choose only one to be active!
+    
     # save image:
     l = cv2.waitKey(5) & 0xFF
     if l == 115: #s
@@ -107,7 +141,8 @@ while True:
     # k = cv2.waitKey(5) & 0xFF
     # if k == 27:
     #     break
-    sleep (0.01)
+    
+    sleep (0.01) # so the pi won't crush and cut the connection
 
 cv2.destroyAllWindows()
 visionManager.cam.release()
