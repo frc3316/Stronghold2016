1551b5284fcc776bb41a29593d331aa98ba1cc32
laptop <frc3316@gmail.com>
2016-01-26 23:12:16 +0200

    Done profiling with constant accel, now working on combining step lists

--- Robot Code/src/org/usfirst/frc/team3316/robot/chassis/motion/MotionPlanner.java
+++ Robot Code/src/org/usfirst/frc/team3316/robot/chassis/motion/MotionPlanner.java
@@ -11,94 +11,170 @@ import org.usfirst.frc.team3316.robot.logger.DBugLogger;
 
 import com.sun.java_cup.internal.runtime.virtual_parse_stack;
 
-public class MotionPlanner 
+public class MotionPlanner
 {
 	public static class Step
 	{
 		double accel, velocity, position, time;
-		
-		public Step (double accel, double velocity, double position, double time)
+
+		public Step(double accel, double velocity, double position, double time)
 		{
 			this.accel = accel;
 			this.velocity = velocity;
 			this.position = position;
 			this.time = time;
 		}
-		
-		public Step (Step other)
+
+		public Step(Step other)
 		{
 			this.accel = other.accel;
 			this.velocity = other.velocity;
 			this.position = other.position;
 			this.time = other.time;
 		}
-	}	
-	
+	}
+
 	public static class PlannedMotion
 	{
-		
+
 	}
-	
+
 	static Config config = Robot.config;
 	static DBugLogger logger = Robot.logger;
-	
-	static double maxAccel, maxDeccel, maxVelocity, timeStep;
-	
+
+	static double maxAccel, maxdecel, maxVelocity, timeStep;
+
 	static
 	{
 		maxAccel = (double) config.get("motionPlanner_MaxAccel");
-		maxDeccel = -1 * (double) config.get("motionPlanner_MaxDeccel");
+		maxdecel = -1 * (double) config.get("motionPlanner_Maxdecel");
 		maxVelocity = (double) config.get("motionPlanner_MaxVelocity");
 		timeStep = (double) config.get("motionPlanner_TimeStep");
 	}
-	
-	public static PlannedMotion planMotion (double distance)
+
+	public static Step[] planMotion(double distance)
 	{
-		ArrayList <Step> accelList = calculateAccelSteps(maxVelocity, maxAccel);
-		ArrayList <Step> deccelList = calculateAccelSteps(maxVelocity, maxDeccel);
-		
+		ArrayList<Step> accelList = calculateAccelSteps(maxVelocity, maxAccel);
+		ArrayList<Step> decelList = calculateAccelSteps(maxVelocity, maxdecel);
+
+		flipdecelList(decelList, distance);
+
+		double accelDistance = accelList.get(accelList.size() - 1).position
+				- accelList.get(0).position; // how much we go in the accel part
+
+		double decelDistance = decelList.get(decelList.size() - 1).position
+				- decelList.get(0).position; // how much we go in the decel
+												// part
+
+		/*
+		 * Distances combined are what we were looking for? Combining is easy!
+		 */
+		if (accelDistance + decelDistance == distance)
+		{
+			ArrayList<Step> finalList = addTwoStepLists(accelList, decelList);
+
+			return finalList.toArray(new Step[0]);
+		}
+		/*
+		 * Distance is too much, need to trim lists when velocities are equal and total distance is okay
+		 */
+		else if (accelDistance + decelDistance > distance)
+		{
+			for (Step step : accelList)
+			{
+				
+			}
+		}
+		/*
+		 * Distance is too little, need to add a part of constant velocity
+		 */
+		else
+		{
+
+		}
+
 		return null;
 	}
-	
-	private static ArrayList<Step> calculateAccelSteps (double maxVelocity, double accel)
+
+	/**
+	 * Given a max velocity and a constant acceleration, returns a list of the
+	 * steps required to reach that max velocity. Assumes velocity and
+	 * acceleration are positive and starting at position and velocity 0.
+	 */
+	private static ArrayList<Step> calculateAccelSteps(double maxVelocity,
+			double accel)
 	{
 		ArrayList<Step> steps = new ArrayList<>();
-		
-		Step currentStep = new Step (0, 0, accel, 0);
+
+		Step currentStep = new Step(0, 0, accel, 0);
 		steps.add(new Step(currentStep));
-		
+
 		while (currentStep.velocity < maxVelocity)
 		{
-			currentStep.accel = accel; //Trapezoid profile
-			
+			currentStep.accel = accel; // Trapezoid profile
+
 			currentStep.velocity += currentStep.accel * timeStep;
-			
-			if (currentStep.velocity > maxVelocity) 
+
+			if (currentStep.velocity > maxVelocity)
 			{
-				currentStep.velocity = maxVelocity; 
+				currentStep.velocity = maxVelocity;
 			}
-			
+
 			currentStep.position += currentStep.velocity * timeStep;
 
 			currentStep.time += timeStep;
-			
+
 			steps.add(new Step(currentStep));
 		}
 		return steps;
 	}
-	
-	private static void flipDeccelList (ArrayList<Step> deccelList, double maxPosition)
+
+	/**
+	 * Flips a list of accelerating steps into decelerating steps.
+	 * 
+	 * @param decelList
+	 *            The list to flip.
+	 * @param maxPosition
+	 *            The final distance of the total motion.
+	 */
+	private static void flipdecelList(ArrayList<Step> decelList,
+			double maxPosition)
 	{
-		double maxVelocity = deccelList.get(deccelList.size() - 1).velocity; //velocity of last step
-		
+		double maxVelocity = decelList.get(decelList.size() - 1).velocity; // velocity
+																			// of
+																			// last
+																			// step
+
 		/*
-		 * Flipping distances and velocities
+		 * Flipping distances, velocities and acceleration
 		 */
-		for (Step step : deccelList)
+		for (Step step : decelList)
 		{
 			step.position = maxPosition - step.position;
 			step.velocity = maxVelocity - step.velocity;
-			step.accel
+			step.accel *= -1; // flip acceleration sign because positive
+								// acceleration was used for creating the list
 		}
+	}
+
+	/**
+	 * Adds 2 lists so that their time steps are coordinated.
+	 * 
+	 * @return A reference to the first list (which is now the combined list).
+	 *         Ruins the 2 lists in the process.
+	 */
+	private static ArrayList<Step> addTwoStepLists(ArrayList<Step> firstList,
+			ArrayList<Step> lastList)
+	{
+		double firstTime = firstList.get(firstList.size() - 1).time;
+
+		for (Step step : lastList)
+		{
+			step.time += firstTime;
+		}
+
+		firstList.addAll(lastList);
+
+		return firstList;
 	}
 }
