4d12b3809ecc2eff4c4737ab7284b7bdebd67994
Programming PC <frc3316@gmail.com>
2016-02-05 12:11:36 +0200

    Fixes to subsystems

--- Robot Code/src/org/usfirst/frc/team3316/robot/subsystems/Turret.java
+++ Robot Code/src/org/usfirst/frc/team3316/robot/subsystems/Turret.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team3316.robot.subsystems;
 
 import org.usfirst.frc.team3316.robot.Robot;
+import org.usfirst.frc.team3316.robot.commands.turret.StopTurret;
 import org.usfirst.frc.team3316.robot.robotIO.DBugSpeedController;
 
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
@@ -20,22 +21,31 @@ public class Turret extends DBugSubsystemCC
 
 	public void initDefaultCommand()
 	{
+		setDefaultCommand(new StopTurret());
 	}
 
 	/**
-	 * Set the voltage for the motors of this subsystem.
+	 * Set the voltage for the motors of this subsystem. If the turret is past
+	 * the allowed position (about to break itself) it allows movement only in
+	 * one direction.
+	 * 
 	 * @par v - The voltage for the motors (between 1.0 to -1.0).
 	 */
 	public boolean setMotors(double v)
 	{
-		// If the turret is able to move.
-		if (getAngle() > (double) config.get("turret_Pot_LowThresh")
-				&& getAngle() < (double) config.get("turret_Pot_HighThresh")) {
-			return super.setMotors(v);
+		// If the turret is able to move. We assume positive is for CW movement
+		// and negative is for CCW movement.
+
+		if (getAngle() > (double) config.get("turret_Pot_RightThresh"))
+		{
+			v = Math.min(v, 0);
 		}
-		
-		super.setMotors(0);
-		return false;
+		else if (getAngle() < (double) config.get("turret_Pot_LeftThresh"))
+		{
+			v = Math.max(v, 0);
+		}
+
+		return super.setMotors(v);
 	}
 
 	/**
