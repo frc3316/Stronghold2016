ec9ccef4f53ebcbd651deab3ebbad4a9935bb13b
frc3316 <frc3316@gmail.com>
2016-02-07 22:48:46 +0200

    Removed AutonomousSequence file content

--- Robot Code/src/org/usfirst/frc/team3316/robot/sequences/AutonomousSequence.java
+++ Robot Code/src/org/usfirst/frc/team3316/robot/sequences/AutonomousSequence.java
@@ -6,10 +6,6 @@ import org.usfirst.frc.team3316.robot.commands.chassis.auton.CrossDefense;
 
 public class AutonomousSequence extends DBugCommandGroup {
 	public AutonomousSequence() {
-		addSequential(new CrossDefense(false));
-		//addSequential(new DriveDistance(0.2, false));
-		//addSequential(new ShootingSequence());
-		addSequential(new CrossDefense(true));
-		//addSequential(new DriveDistance(0.2, true));
+
 	}
 }
