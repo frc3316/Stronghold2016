09cc4ddf5f92b8c5ace02a3799650297169199e8
laptop <frc3316@gmail.com>
2016-02-16 00:07:06 +0200

    Put value from joystick in smart dashboard in a digusting manner

--- Robot Code/src/org/usfirst/frc/team3316/robot/commands/climbing/JoystickWinchControl.java
+++ Robot Code/src/org/usfirst/frc/team3316/robot/commands/climbing/JoystickWinchControl.java
@@ -3,6 +3,8 @@ package org.usfirst.frc.team3316.robot.commands.climbing;
 import org.usfirst.frc.team3316.robot.Robot;
 import org.usfirst.frc.team3316.robot.commands.DBugCommand;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class JoystickWinchControl extends DBugCommand
 {
 	public JoystickWinchControl ()
@@ -17,6 +19,7 @@ public class JoystickWinchControl extends DBugCommand
 	{
 		isFin = !Robot.climbing.setMotors(-Robot.joysticks.joystickOperator.getRawAxis(5)); // Coz flight
 																							// simulators
+		SmartDashboard.putNumber("Operator axis 5 value", -Robot.joysticks.joystickOperator.getRawAxis(5));
 	}
 
 	protected boolean isFinished()
