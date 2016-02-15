9700ace8977ea5168323e086e1f1306012016e13
Programming PC <frc3316@gmail.com>
2016-02-15 19:13:06 +0200

    Added shooting sequence

---
+++ Robot Code/src/org/usfirst/frc/team3316/robot/sequences/ShootingSequence.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team3316.robot.sequences;
+
+import org.usfirst.frc.team3316.robot.commands.DBugCommandGroup;
+
+public class ShootingSequence extends DBugCommandGroup
+{
+	public ShootingSequence()
+	{
+	}
+
+}
