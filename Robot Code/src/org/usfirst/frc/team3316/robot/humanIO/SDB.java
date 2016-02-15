f577c7898992463f4993238a31443def90f6a370
Programming PC <frc3316@gmail.com>
2016-02-15 18:24:46 +0200

    Removed garbage from SDB

--- Robot Code/src/org/usfirst/frc/team3316/robot/humanIO/SDB.java
+++ Robot Code/src/org/usfirst/frc/team3316/robot/humanIO/SDB.java
@@ -9,52 +9,17 @@ import java.util.Set;
 import java.util.TimerTask;
 
 import org.usfirst.frc.team3316.robot.Robot;
-import org.usfirst.frc.team3316.robot.commands.chassis.ExtendOmni;
-import org.usfirst.frc.team3316.robot.commands.chassis.RetractOmni;
-import org.usfirst.frc.team3316.robot.commands.chassis.TankDrive;
-import org.usfirst.frc.team3316.robot.commands.intake.StopRoll;
-import org.usfirst.frc.team3316.robot.commands.intake.WaitForBallIn;
-import org.usfirst.frc.team3316.robot.commands.intake.WaitForBallOut;
-import org.usfirst.frc.team3316.robot.commands.chassis.ToggleOmni;
-import org.usfirst.frc.team3316.robot.commands.chassis.WaitForDefense;
-import org.usfirst.frc.team3316.robot.commands.chassis.auton.CrossDefense;
-import org.usfirst.frc.team3316.robot.commands.chassis.auton.Direction;
 import org.usfirst.frc.team3316.robot.commands.hood.HoodBangbang;
 import org.usfirst.frc.team3316.robot.commands.hood.HoodJoysticks;
 import org.usfirst.frc.team3316.robot.commands.hood.HoodPID;
-import org.usfirst.frc.team3316.robot.commands.hood.StopHood;
-import org.usfirst.frc.team3316.robot.commands.hood.ZeroAngle;
-import org.usfirst.frc.team3316.robot.commands.intake.CloseIntake;
-import org.usfirst.frc.team3316.robot.commands.intake.OpenIntake;
-import org.usfirst.frc.team3316.robot.commands.intake.RollIn;
-import org.usfirst.frc.team3316.robot.commands.intake.RollOut;
-import org.usfirst.frc.team3316.robot.commands.transport.BangbangTransport;
-import org.usfirst.frc.team3316.robot.commands.turret.StopTurret;
-import org.usfirst.frc.team3316.robot.commands.turret.TurretBangbang;
-import org.usfirst.frc.team3316.robot.commands.turret.TurretJoysticks;
-import org.usfirst.frc.team3316.robot.commands.turret.TurretPID;
-import org.usfirst.frc.team3316.robot.commands.climbing.ReleaseArmPiston;
-import org.usfirst.frc.team3316.robot.commands.climbing.ReleaseDown;
-import org.usfirst.frc.team3316.robot.commands.climbing.StopWinch;
-import org.usfirst.frc.team3316.robot.commands.climbing.WaitForRung;
-import org.usfirst.frc.team3316.robot.commands.climbing.JoystickWinchControl;
-import org.usfirst.frc.team3316.robot.commands.climbing.PullUp;
-import org.usfirst.frc.team3316.robot.commands.climbing.lockArmPiston;
 import org.usfirst.frc.team3316.robot.commands.flywheel.BangbangFlywheel;
 import org.usfirst.frc.team3316.robot.commands.flywheel.JoystickFlywheel;
-import org.usfirst.frc.team3316.robot.commands.flywheel.PIDFlywheel;
 import org.usfirst.frc.team3316.robot.config.Config;
 import org.usfirst.frc.team3316.robot.logger.DBugLogger;
-import org.usfirst.frc.team3316.robot.sequences.CrossingBackSequence;
-import org.usfirst.frc.team3316.robot.sequences.CrossingForwardSequence;
 
 import org.usfirst.frc.team3316.robot.vision.VisionServer;
 
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.livewindow.LiveWindowSendable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.usfirst.frc.team3316.robot.sequences.ClimbingSequence;
 
 public class SDB
 {
