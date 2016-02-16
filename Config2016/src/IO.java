2c0f856c8482b1096a1610ba56100a815d71efcb
laptop <frc3316@gmail.com>
2016-02-16 17:49:33 +0200

    Updated solenoid channels

--- Config2016/src/IO.java
+++ Config2016/src/IO.java
@@ -305,10 +305,10 @@ public class IO
 				Config.addToConstants("CHASSIS_LONG_PISTONS_REVERSE", 1);
 
 				Config.addToConstants("CHASSIS_SHORT_PISTONS_LEFT_FORWARD", 2);
-				Config.addToConstants("CHASSIS_SHORT_PISTONS_LEFT_REVERSE", 3);
+				Config.addToConstants("CHASSIS_SHORT_PISTONS_LEFT_REVERSE", 5);
 
-				Config.addToConstants("CHASSIS_SHORT_PISTONS_RIGHT_FORWARD", 4);
-				Config.addToConstants("CHASSIS_SHORT_PISTONS_RIGHT_REVERSE", 5);
+				Config.addToConstants("CHASSIS_SHORT_PISTONS_RIGHT_FORWARD", 6);
+				Config.addToConstants("CHASSIS_SHORT_PISTONS_RIGHT_REVERSE", 7);
 
 				// Module 0
 				Config.addToConstants("INTAKE_SOLENOID_MODULE", 0);
