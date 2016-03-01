6c5402c456a46aa6b3389b2c908e14afa8f28c0e
Programming PC <frc3316@gmail.com>
2016-03-01 22:34:13 +0200

    Fixed open/close intake transport to be more generic

--- Robot Code/src/org/usfirst/frc/team3316/robot/commands/intake/CloseIntakeTransport.java
+++ Robot Code/src/org/usfirst/frc/team3316/robot/commands/intake/CloseIntakeTransport.java
@@ -6,30 +6,44 @@ import org.usfirst.frc.team3316.robot.commands.transport.TransportRollOut;
 public class CloseIntakeTransport extends DBugCommand
 {
 	private DBugCommand closeIntake, transportRollOut;
-	
-	public CloseIntakeTransport() {
+
+	private boolean closeStarted;
+	// This boolean is to prevent this command from ending in case that the close intake hasn't started yet
+	// and this command's isFinished is invoked
+
+	public CloseIntakeTransport()
+	{
 		closeIntake = new CloseIntake();
 		transportRollOut = new TransportRollOut();
 	}
-	
+
 	protected void init()
 	{
+		closeStarted = false;
+
 		closeIntake.start();
 		transportRollOut.start();
 	}
-	
+
 	protected void execute()
-	{}
-	
-	protected boolean isFinished() {
-		return timeSinceInitialized() >= (double) config.get("intake_CloseIntake_Timeout");
+	{
+		if (closeIntake.isRunning())
+		{
+			closeStarted = true;
+		}
 	}
-	
-	protected void fin() {
+
+	protected boolean isFinished()
+	{
+		return !closeIntake.isRunning() && closeStarted;
+	}
+
+	protected void fin()
+	{
 		closeIntake.cancel();
 		transportRollOut.cancel();
 	}
-	
+
 	protected void interr()
 	{
 		fin();
--- Robot Code/src/org/usfirst/frc/team3316/robot/commands/intake/OpenIntakeTransport.java
+++ Robot Code/src/org/usfirst/frc/team3316/robot/commands/intake/OpenIntakeTransport.java
@@ -6,30 +6,44 @@ import org.usfirst.frc.team3316.robot.commands.transport.TransportRollIn;
 public class OpenIntakeTransport extends DBugCommand
 {
 	private DBugCommand openIntake, transportRollIn;
-	
-	public OpenIntakeTransport() {
+
+	private boolean openStarted;
+	// This boolean is to prevent this command from ending in case that the open intake hasn't started yet and
+	// this command's isFinished is invoked
+
+	public OpenIntakeTransport()
+	{
 		openIntake = new OpenIntake();
 		transportRollIn = new TransportRollIn();
 	}
-	
+
 	protected void init()
 	{
+		openStarted = false;
+
 		openIntake.start();
 		transportRollIn.start();
 	}
-	
+
 	protected void execute()
-	{}
-	
-	protected boolean isFinished() {
-		return timeSinceInitialized() >= (double) config.get("intake_OpenIntake_Timeout");
+	{
+		if (openIntake.isRunning())
+		{
+			openStarted = true;
+		}
 	}
-	
-	protected void fin() {
+
+	protected boolean isFinished()
+	{
+		return !openIntake.isRunning() && openStarted;
+	}
+
+	protected void fin()
+	{
 		openIntake.cancel();
 		transportRollIn.cancel();
 	}
-	
+
 	protected void interr()
 	{
 		fin();
