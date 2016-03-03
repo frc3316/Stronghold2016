c4639b58a9559448afa9c215e21bd1846e573e7d
roey89 <roey8999@gmail.com>
2016-03-03 03:03:05 +0200

    Fixed distannce caluclation in DistanceHelper with go_magic

--- Raspberry Pi/RaspberrySide/VisionManager.py
+++ Raspberry Pi/RaspberrySide/VisionManager.py
@@ -1,263 +1,214 @@
 import cv2
 import numpy as np
 from Constants import *
-from AngleHelper import *
 from DistanceHelper import *
-from ImageObject import *
-from RobotObject import *
 from Utils import *
-from math import cos, degrees, radians
 from collections import namedtuple
 
-BoundingRect = namedtuple('BoundingRect', ('contour', 'width', 'height', 'x', 'y'))
+BoundingRect = namedtuple('BoundingRect', ('contour', 'width', 'height', 'x_offset', 'y_offset'))
+ImageObject = namedtuple('ImageObject', ('tower_distance', 'azimuth_angle', 'polar_angle'))
+
 
 class VisionManager(object):
-    '''
+    """
     A class that manages all the computer vision for the FRC 2016.
-    '''
+    """
     READ_BUFFER_AMOUNT = 4  # Amount of frames to read in each update in order to clear buffer
 
-    def __init__(self,minColor,maxColor,minimumBoundingRectSize,cam,knownHeight,knownWidth,focalLength,
-                 robotMeasurements,TOWER_HEIGHT,centerUWidth,currentUWidthDistance,HAX,HAY):
-        '''
-        This method initialising the variables for the VisionManager instance.
-        :param minColor: The min color to be passed in the mask. (in the format: np.array([B,G,R])).
-        :param maxColor: The max color to be passed in the mask. (in the format: np.array([B,G,R])).
-        :param minimumBoundingRectSize: The minimum contour area to be considered as an object.
-        :param cam: A cv2.VideoCapture instance, to be used as the input camera.
-        :param robotMeasurements: The measurements of the robot, in the format:
-        - robotMeasurements[0] is the robot height
-        - robotMeasurements[1] is the robot width
-        - robotMeasurements[2] is the robot length
-
-        MARK: AngleHelper, DistanceHelper init vars.
-        :param focalLength: The focal length of the object (U).
-        :param knownHeight: The real life height of the object (U).
-        :param knownWidth: The real life width of the object (U).
-        :param TOWER_HEIGHT: The height of the tower.
-        :param centerUWidth: The U width as it looks in the camera when it is in the center (in pixels).
-        :param currentUWidthDistance: The distance which the centerUWidth was calculated from.
-        :param HAX: The head angle of the camera (x).
-        :param HAY: The head angle of the camera (y).
-        :return: None
-        '''
-
-        self.angleHelper = AngleHelper(knownWidth,knownHeight,centerUWidth,currentUWidthDistance)
-        self.distanceHelper = DistanceHelper(knownHeight,focalLength)
-        self.TOWER_HEIGHT = TOWER_HEIGHT
-        self.KNOWN_HEIGHT = knownHeight
-        self.KNOWN_WIDTH = knownWidth
-        # MARK: Images.
+    def __init__(self):
         self.currentImage = None
         self.maskedImage = None
-        self.threshImage = None
-        self.isObjectDetected = False # is self detected an object (U).
-        # Mark: Scales.
-        self.currentImageObject = None # The current biggest Object in the masked image (probably the tower).
-        self.imageHeight = None
-        self.imageWidth = None
+        self.cam = VisionManager.init_camera()
 
-        self.minimumBoundingRectSize = minimumBoundingRectSize
-        self.minColor = minColor
-        self.maxColor = maxColor
+    @staticmethod
+    def init_camera():
+        cam = cv2.VideoCapture(getCameraNumber())
+        if not cam.isOpened():
+            raise ValueError("Camera Not Found")
 
-        self.cam = cam
-        self.robotObject = RobotObject(robotMeasurements[0],robotMeasurements[1],robotMeasurements[2])
+        if not cam.set(3, RESIZE_IMAGE_WIDTH):
+            logger.warning("Set width failed")
 
-        self.focalLength = focalLength
-        self.HAX = HAY # because we are flipping the image.
-        self.HAY = HAX
+        if not cam.set(4, RESIZE_IMAGE_HEIGHT):
+            logger.warning("Set height failed")
 
-    def __setImageScales(self):
-        '''
-        This private method sets the scales of the image(self.currentImage).
-        This method is auto called, and should not be called manually.
-        :return: None
-        '''
+        if not cam.set(cv2.cv.CV_CAP_PROP_BRIGHTNESS, BRIGHTNESS):
+            logger.warning("Set brightness failed")
 
-        height, width = self.currentImage.shape[:2]
-        self.imageHeight = height
-        self.imageWidth = width
+        if not cam.set(cv2.cv.CV_CAP_PROP_SATURATION, SATURATION):
+            logger.warning("Set saturation failed")
 
-    def updateImage(self):
-        '''
+        if not cam.set(cv2.cv.CV_CAP_PROP_EXPOSURE, EXPOSURE):
+            logger.warning("Set exposure failed")
+
+        if not cam.set(cv2.cv.CV_CAP_PROP_CONTRAST, CONTRAST):
+            logger.warning("Set contrast failed")
+
+        return cam
+
+    def capture_frame(self):
+        """
         This method updates the current image of the VisionManager instance.
         :return: None.
-        '''
+        """
 
-        if self.isObjectDetected:
-            self.currentImageObject.didUpdateVar = False	
-
-        didGetImage, new_frame = self.cam.read()
-        if not didGetImage:  # Can't event grab one frame - might be a problem
-             logger.error("Couldn't Read Image from self.cam!")
-             return
-
-        logger.info("Got frame shape: %r" % (new_frame.shape,))
+        got_image, new_frame = self.cam.read()
+        if not got_image:  # Can't event grab one frame - might be a problem
+            logger.error("Couldn't Read Image from self.cam!")
+            return
 
         for _ in xrange(self.READ_BUFFER_AMOUNT - 1):  # minus one, since we already grabbed one frame.
-             frame = new_frame
-             didGetImage, new_frame = self.cam.read()  # Grab another frame
-             
-             if not didGetImage:
-                 logger.debug("Seems like the buffer is empty")
-                 break  # Seems like the buffer is empty - lets use the last frame
-
-        else:  # this occours if the loop wasn't broken - lets use the new frame
-            logger.debug("buffer isn't empty")
             frame = new_frame
-            
-        # maybe resize changes edges of u? causing the u to be smaller of bigger
-        #frame = cv2.resize(frame, (resizedImageWidth,resizedImageHeight))
-        frame = self.rotateImage(frame)
-    
-        self.currentImage = frame
-        if self.currentImage == None or self.imageHeight == None or self.imageWidth == None:
-            self.__setImageScales()
+            got_image, new_frame = self.cam.read()  # Grab another frame
 
+            if not got_image:
+                break  # Seems like the buffer is empty - lets use the last frame
 
-    def rotateImage(self,img):
-        '''
-        This method rotates the image img by 90 degrees, if Constants.rotateClockwise is True, we will rotate
-        clockwise, else anticlockwise.
-        :param img: the image to be rotated.
-        :return: A new image, the rotated image of img.
-        '''
-        if rotateClockwise:
-            return np.rot90(img,1)
-        else:
-            return np.rot90(img,3)
+        else:  # this occurs if the loop wasn't broken - lets use the new frame
+            frame = new_frame
 
-    def updateMaskThresh(self):
-        '''
-        This method updates self.maskedImage, self.threshImage, using self.currentImage.
-        :return: None
-        '''
+        return np.rot90(frame, 1) if ROTATE_CLOCKWISE else np.rot90(frame, 3)
 
-        hsv = cv2.cvtColor(self.currentImage, cv2.COLOR_BGR2HSV)
+    @staticmethod
+    def get_masked_frame(frame):
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
         # Threshold the HSV image to get only blue colors.
-        mask = cv2.inRange(hsv, self.minColor, self.maxColor)
+        return cv2.inRange(hsv, LOWER_COLOR_BOUND, UPPER_COLOR_BOUND)
 
-        # Bitwise-AND mask and original image.
-        res = cv2.bitwise_and(self.currentImage, self.currentImage, mask=mask)
-        self.maskedImage = res
-
-        thresh = cv2.threshold(mask, 25, 255, cv2.THRESH_BINARY)[1]
+    @staticmethod
+    def get_masked_frame_threshold(masked_frame):
+        """
+        This method updates self.maskedImage, self.threshImage, using self.currentImage.
+        :return: None
+        """
+        thresh = cv2.threshold(masked_frame, 25, 255, cv2.THRESH_BINARY)[1]
         # dilate the threshed image to fill in holes, then find contours on thresholded image.
-        thresh = cv2.dilate(thresh, None, iterations = 0)
-        self.threshImage = thresh
+        thresh = cv2.dilate(thresh, None, iterations=0)
 
-    def calculateBoundingRects(self):
-        '''
+        return thresh
+
+    def get_bounding_rects(self, thresh):
+        """
         This method calculates the bounding rect of the biggest object that matches the min-max colors in
         self.current_image.
         :return: None if no object, else the bounding rect in the format of (x, y, w, h).
-        '''
-
-        self.updateMaskThresh()
-        thresh = self.threshImage
-        (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        if cnts:
-            for c in cnts:
-                if cv2.contourArea(c) < self.minimumBoundingRectSize:
-                    continue
-
-                if cv2.contourArea(c) > maximumBoundingRectangle:
-                    continue
-                (x, y, w, h) = cv2.boundingRect(c)
-                if w == 0:
-                    continue
-                
-                ratio = h/w
-                if HWR < ratio:
-                    continue
-
-                yield BoundingRect(contour=c, height=h, width=w, x=x, y=y)
-
-    @staticmethod
-    def filterBoundingRects(boundingRects):
         """
-        Filters the bounding rectangle with the largest area.
-        :param boundingRects: List of bounding rectangles
-        :return: single bounding rectangle tuple
-        """
-        # Sort the list by <rectangle_area> from large to small and return the largest value
-        return sorted(boundingRects, key=lambda r: r.height * r.width, reverse=True)[0]
 
-    def updateTowerScales(self):
-        '''
+        (contours, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        if not contours:
+            return []
+
+        rects = []
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if (MIN_BOUND_RECT_AREA > area) or (MAX_BOUND_RECT_AREA < area):
+                logger.debug("Rect denied, min-max rect area")
+                continue
+
+            (x_offset, y_offset, width, height) = cv2.boundingRect(contour)
+            ratio = height / width
+            if (MIN_HIGHT_WIDTH_RATIO > ratio) or (MIN_HIGHT_WIDTH_RATIO < ratio):
+                logger.debug("Rect denied, min-max ratio")
+                continue
+
+            rects.append(
+                BoundingRect(contour=contour, height=height, width=width, x_offset=x_offset, y_offset=y_offset))
+
+        return rects
+
+    def get_goal_image(self, save_frame_path=None, save_mask_path=None):
+        """
         This method updates the vars holding the scales and distance from camera of the object.
         :return: None
-        '''
-        self.isObjectDetected = False
+        """
+        frame = self.capture_frame()
+        if save_frame_path:
+            logger.debug("Writing frame to path: %s", save_frame_path)
+            cv2.imwrite(save_frame_path, frame)
 
-        # Filter rectangles
-        boundingRects = list(self.calculateBoundingRects())
+        mask = VisionManager.get_masked_frame(frame)
+        thresh = VisionManager.get_masked_frame_threshold(mask)
 
-        if len(boundingRects) == 0:
+        bounding_rects = self.get_bounding_rects(thresh)
+        if len(bounding_rects) == 0:
             # There are no rects to calculate area for
+            logger.debug("No rects to calculate area for")
+            if save_mask_path:
+                try:
+                    os.remove(save_mask_path)
+                except OSError, ex:
+                    pass
             return
 
-        bestBoundingRect = self.filterBoundingRects(boundingRects)
+        bounding_rects = sorted(bounding_rects, key=lambda r: r.height * r.width, reverse=True)
+        best_bounding_rect = bounding_rects[0]
 
+        min_area_bounding_rect = cv2.minAreaRect(best_bounding_rect.contour)
 
-        if bestBoundingRect is None:
+        try:
+            azimuth_angle = get_azimuth_angle(x_offset=best_bounding_rect.x_offset,
+                                              image_width=best_bounding_rect.width,
+                                              frame_width=frame.shape[1],
+                                              viewing_angle=CAMERA_VIEW_ANGLE_X)
+
+            polar_angle = get_polar_angle(y_offset=best_bounding_rect.y_offset,
+                                          image_height=best_bounding_rect.height,
+                                          frame_height=frame.shape[0],
+                                          viewing_angle=CAMERA_VIEW_ANGLE_Y)
+
+            normalize_skew = normalize_rectangle_skew(
+                bounding_rectangle_height=best_bounding_rect.height,
+                bounding_rectangle_width=best_bounding_rect.width,
+                min_area_bounding_rectangle_angle=min_area_bounding_rect[2])
+
+            actual_height = normalize_rectangle_keystone(
+                bounding_rectangle_height=normalize_skew,
+                polar_angle=polar_angle,
+                camera_pitch=CAMERA_PITCH)
+
+            best_bounding_rect = BoundingRect(contour=best_bounding_rect.contour,
+                                              height=actual_height,
+                                              width=best_bounding_rect.width,
+                                              x_offset=best_bounding_rect.x_offset,
+                                              y_offset=best_bounding_rect.y_offset)
+
+            distance_from_tower = get_distance_from_tower(image_height=best_bounding_rect.height,
+                                                          object_height=OBJECT_HEIGHT,
+                                                          enlargement_factor=ENLARGEMENT_FACTOR)
+            logger.debug("Distance From Camera: " + str(distance_from_tower))
+        except ValueError, ex:
+            logger.warning("Failed normalizing rectangle height: %s", ex.message)
             return
 
-        self.isObjectDetected = True
+        logger.debug("save mask path " + str(save_mask_path))
+        if save_mask_path:
+            writable_mask = cv2.bitwise_and(frame, frame, mask=mask)
+            self.draw_target_square(frameToDraw=writable_mask,
+                                    x_offset=best_bounding_rect.x_offset,
+                                    y_offset=best_bounding_rect.y_offset,
+                                    height=best_bounding_rect.height,
+                                    width=best_bounding_rect.width)
 
-        min_rect = cv2.minAreaRect(bestBoundingRect.contour)
-        min_rect_angle = min_rect[2]
+            cv2.imwrite(save_mask_path, writable_mask)
 
-        actual_height = self.distanceHelper.normalize_rectangle_height(bestBoundingRect, min_rect_angle)
-
-        if actual_height == 3316: # error code of normalize_rectangle_height function
-            self.isObjectDetected = False
-            logger.warning("Error code in normalize_rectangle_height function")
-            return
-
-        distance_from_tower = self.distanceHelper.getDistanceFromTower(actual_height,
-                                                                       self.robotObject, self.TOWER_HEIGHT)
+        return ImageObject(tower_distance=distance_from_tower,
+                           azimuth_angle=azimuth_angle,
+                           polar_angle=polar_angle)
 
 
-        # Changing the object height according to the camera angle
-        if self.currentImageObject is None:
-            self.currentImageObject = ImageObject(bestBoundingRect.width, actual_height,
-                                                  bestBoundingRect.x, bestBoundingRect.y, distance_from_tower)
-
-        else:
-            self.currentImageObject.objectHeight = actual_height
-            self.currentImageObject.objectWidth = bestBoundingRect.width
-            self.currentImageObject.objectX = bestBoundingRect.x
-            self.currentImageObject.objectY = bestBoundingRect.y
-            self.currentImageObject.distanceFromCamera = distance_from_tower
-
-        azimuthalAngle = self.distanceHelper.getAzimuthalAngle([self.imageWidth, self.imageHeight],
-                                                               self.currentImageObject, self.HAX)
-
-        polarAngle = self.distanceHelper.getPolarAngle([self.imageWidth, self.imageHeight],
-                                                       self.currentImageObject, self.HAY)
-
-        self.currentImageObject.azimuthalAngle = azimuthalAngle
-        self.currentImageObject.polarAngle = polarAngle
-
-        self.currentImageObject.didUpdateVar = True
-
-    def updateRobotScales(self):
-        '''
-        This method updates the self.robotObject vars.
+    def draw_target_square(self, frameToDraw, x_offset, y_offset, height, width, color=(0, 0, 255), stroke=2):
+        """
+        Draws a red rectangle around the detected goal image
+        :param frameToDraw: the frame to draw the rectangle on
+        :param x_offset: offset x from left of frame
+        :param y_offset: offset y from top of frame
+        :param height: height of goal image
+        :param width: width of goal image
+        :param color: color of rectangle (default: red)
+        :param stroke: stroke width (default: 2px)
         :return: None
-        '''
-
-        if self.isObjectDetected:
-            if not self.currentImageObject.didUpdateVar:
-                self.updateTowerScales()
-                self.currentImageObject.didUpdateVar = True
-            self.robotObject.distanceFromTower = self.currentImageObject.distanceFromCamera
-            self.robotObject.angle = self.angleHelper.getAngle(self.currentImageObject.distanceFromCamera,
-                                                            self.currentImageObject.objectWidth,
-                                                            self.focalLength, self.currentImageObject.objectHeight)
-            self.robotObject.XPosition = self.distanceHelper.getXRobotPosition(self.robotObject)
-            self.robotObject.Yposition = self.distanceHelper.getYRobotPosition(self.robotObject)
+        """
+        cv2.rectangle(frameToDraw, (int(x_offset), int(y_offset)), (int(x_offset + width), int(y_offset + height)),
+                      color, stroke)
