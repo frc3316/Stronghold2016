a6157ab4b44fb3ad2cf98bad5b7777ccc907a4f0
NEW LAPTOP <frc3316@gmail.com>
2016-03-06 21:33:14 +0200

    Added Autonomous sequecne with shooting

---
+++ Robot Code/src/org/usfirst/frc/team3316/robot/sequences/AutonomousShootingSequence.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team3316.robot.sequences;
+
+import org.usfirst.frc.team3316.robot.commands.DBugCommandGroup;
+import org.usfirst.frc.team3316.robot.commands.chassis.RetractOmni;
+import org.usfirst.frc.team3316.robot.commands.chassis.auton.Direction;
+import org.usfirst.frc.team3316.robot.commands.chassis.auton.ReachDefense;
+
+import edu.wpi.first.wpilibj.command.WaitCommand;
+
+public class AutonomousShootingSequence extends DBugCommandGroup {
+	public AutonomousShootingSequence() {
+		addSequential(new RetractOmni());
+		addSequential(new CrossingForwardSequence());
+		addSequential(new WaitCommand(0.7)); // We want the robot to stop before
+												// targeting the shooter.
+		addSequential(new AutonShootingSequence());
+		addSequential(new ReachDefense(Direction.BACKWARDS));
+	}
+}
