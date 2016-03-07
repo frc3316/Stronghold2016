e534bfe64ff30ae350e848fb1399a83ce87d34bb
roey89 <roey8999@gmail.com>
2016-03-07 18:10:52 +0200

    Added turret PID to autonomous shooting sequence

--- Robot Code/src/org/usfirst/frc/team3316/robot/sequences/AutonomousShootingSequence.java
+++ Robot Code/src/org/usfirst/frc/team3316/robot/sequences/AutonomousShootingSequence.java
@@ -4,12 +4,14 @@ import org.usfirst.frc.team3316.robot.commands.DBugCommandGroup;
 import org.usfirst.frc.team3316.robot.commands.chassis.RetractOmni;
 import org.usfirst.frc.team3316.robot.commands.chassis.auton.Direction;
 import org.usfirst.frc.team3316.robot.commands.chassis.auton.ReachDefense;
+import org.usfirst.frc.team3316.robot.commands.turret.TurretPID;
 
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class AutonomousShootingSequence extends DBugCommandGroup {
 	public AutonomousShootingSequence() {
 		addSequential(new RetractOmni());
+		addParallel(new TurretPID());
 		addSequential(new CrossingForwardSequence());
 		addSequential(new WaitCommand(0.7)); // We want the robot to stop before
 												// targeting the shooter.
