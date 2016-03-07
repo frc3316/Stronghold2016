83a0b4dcf8b3af40e8d8ada8622be25bd3871654
roey89 <roey8999@gmail.com>
2016-03-07 17:29:04 +0200

    Changed how auton trigger shooting works (should work now)

--- Robot Code/src/org/usfirst/frc/team3316/robot/sequences/AutonTriggerShootingSequence.java
+++ Robot Code/src/org/usfirst/frc/team3316/robot/sequences/AutonTriggerShootingSequence.java
@@ -6,38 +6,25 @@ import org.usfirst.frc.team3316.robot.commands.DBugCommandGroup;
 public class AutonTriggerShootingSequence extends DBugCommand
 {
 	private DBugCommandGroup triggerShootingSequence;
-	private int triggerStarted;
-	private int counter = 0;
 	
 	public AutonTriggerShootingSequence ()
 	{
 		triggerShootingSequence = new TriggerShootingSequence();
-		triggerStarted = 0;
 	}
 
 	protected void init()
 	{
-		triggerStarted = 0;
-		counter = 0;
-		
+		setTimeout(3.0);
 	}
 
 	protected void execute()
 	{
-		if (!triggerShootingSequence.isRunning())
-		{
-			triggerShootingSequence.start();
-			counter++;
-		}
-		else
-		{
-			triggerStarted++;
-		}
+		triggerShootingSequence.start();
 	}
 
 	protected boolean isFinished()
 	{
-		return (triggerStarted > 2 && !triggerShootingSequence.isRunning()) || counter > 50;
+		return isTimedOut();
 	}
 
 	protected void fin()
